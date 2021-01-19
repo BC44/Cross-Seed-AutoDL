bbe3b6b7900572300b9e0523fcf7a87524340877
BD <>
2021-01-18 22:28:11 -0500

    re-implement search history, add --ignore-history flag

--- CrossSeedAutoDL.py
+++ CrossSeedAutoDL.py
@@ -12,13 +12,14 @@ from guessit import guessit
 from urllib.parse import urlencode
 
 parser = argparse.ArgumentParser(description='Searches for cross-seedable torrents')
-parser.add_argument('-p', '--parse-dir', dest='parse_dir', action='store_true', help='Will parse the items inside the input directory as individual releases')
+parser.add_argument('-p', '--parse-dir', dest='parse_dir', action='store_true', help='Optional. Indicates whether to parse the items inside the input directory as individual releases')
 parser.add_argument('-d', '--delay', metavar='delay', dest='delay', type=int, default=10, help='Pause duration (in seconds) between searches (default: 10)')
 parser.add_argument('-i', '--input-path', metavar='input_path', dest='input_path', type=str, required=True, help='File or Folder for which to find a matching torrent')
 parser.add_argument('-s', '--save-path', metavar='save_path', dest='save_path', type=str, required=True, help='Directory in which to store downloaded torrents')
 parser.add_argument('-u', '--url', metavar='jackett_url', dest='jackett_url', type=str, required=True, help='URL for your Jackett instance, including port number if needed')
 parser.add_argument('-k', '--api-key', metavar='api_key', dest='api_key', type=str, required=True, help='API key for your Jackett instance')
 parser.add_argument('-t', '--trackers', metavar='trackers', dest='trackers', type=str, default=None, required=False, help='Tracker(s) on which to search. Comma-separated if multiple (no spaces). If ommitted, all trackers will be searched.')
+parser.add_argument('--ignore-history', dest='ignore_history', action='store_true', help='Optional. Indicates whether to ignore history file when conducting searches.')
 ARGS = parser.parse_args()
 
 logger = logging.getLogger(__name__)
@@ -93,7 +94,14 @@ class Searcher:
     def __init__(self):
         self.search_results = []
 
-    def search(self, local_release_data):
+    def search(self, local_release_data, search_history):
+        if not ARGS.ignore_history:
+            if HistoryManager.is_file_previously_searched( local_release_data['basename'], search_history )\
+                    and ARGS.parse_dir:
+                print('Skipping search. File previously searched.')
+                logger.info('Skipping search. File previously searched.')
+                return []
+
         search_query = local_release_data['guessed_data']['title']
         if local_release_data['guessed_data'].get('year', None) is not None:
             search_query += ' ' + str( local_release_data['guessed_data']['year'] )
@@ -101,20 +109,19 @@ class Searcher:
         search_url = self._get_full_search_url(search_query, local_release_data)
         logger.info(search_url)
 
-        # debug
-        # print(search_url);exit()
-
         resp = requests.get(search_url, local_release_data)
-        # debug
-        # print( json.dumps(resp.json(), indent=4) );exit()
+        ###
+        # self._save_results(local_release_data); exit()
         try:
             resp_json = resp.json()
         except json.decoder.JSONDecodeError as e:
+            logger.info(f'Response text: {resp.text}')
             logger.exception(e)
-            logger.info(f'Response text: resp.text')
+            return []
+
+        search_history['basenames_searched'].append(local_release_data['basename'])
 
         self.search_results = self._trim_results( resp_json['Results'] )
-
         return self._get_matching_results(local_release_data)
 
     # construct final search url
@@ -150,12 +157,10 @@ class Searcher:
 
         print(f'{ len(matching_results) } matched of { len(self.search_results) } results.')
         logger.info(f'{ len(matching_results) } matched of { len(self.search_results) } results.')
-        # debug
-        self._save_results(local_release_data)
+
         return matching_results
 
     def _trim_results(self, search_results):
-        url_re = r'^https?://[^/]+(.+)'
         trimmed_results = []
 
         for result in search_results:
@@ -171,26 +176,34 @@ class Searcher:
         release_name_re = r'^(.+?)( \[.*/.*\])?$'
         return re.search(release_name_re, release_name, re.IGNORECASE).group(1)
 
-    # debug
-    def _save_results(self, local_release_data):
-        search_results_path = os.path.join( os.path.dirname(__file__), 'search_results.json' )
-
-        target_dict = {'local_release_data': local_release_data, 'results': self.search_results}
-
-        with open(search_results_path, 'w', encoding='utf8') as f:
-            json.dump([target_dict], f, indent=4)
+    ###
+    # def _save_results(self, local_release_data):
+    #     search_results_path = os.path.join( os.path.dirname(os.path.abspath(__file__)), 'search_results.json' )
+    #     target_dict = {'local_release_data': local_release_data, 'results': self.search_results}
+    #
+    #     with open(search_results_path, 'w', encoding='utf8') as f:
+    #         json.dump([target_dict], f, indent=4)
 
 
 class Downloader:
+    # for the purpose of trimming a 'Description' URL down to its path only. Some trackers might have multiple proxies
+    # ie. http://tracker.url1.net/details?9012 != http://tracker.url2.com/details?9012, but their path remain the same: /details?9012
+
     @staticmethod
-    def download(result):
+    def download(result, search_history):
+        release_name = Downloader._sanitize_name( '{} [{}]'.format( result['Title'], result['Tracker'] ) )
+
         # if torrent file is missing, ie. Blutopia
         if result['Link'] is None:
             print( f'- Skipping release (no download link): {release_name}' )
             logger.info( f'- Skipping release (no download link): {release_name}' )
             return
+        if not ARGS.ignore_history:
+            if HistoryManager.is_torrent_previously_grabbed(result, search_history):
+                print( f'- Skipping download (previously grabbed): {release_name}' )
+                logger.info( f'- Skipping download (previously grabbed): {release_name}' )
+                return
 
-        release_name = Downloader._sanitize_name( '{} [{}]'.format( result['Title'], result['Tracker'] ) )
         file_path = os.path.join( ARGS.save_path, release_name + '.torrent' )
         file_path = Downloader._validate_path(file_path)
 
@@ -200,6 +213,8 @@ class Downloader:
         response = requests.get(result['Link'], stream=True)
         with open(file_path, 'wb') as f:
             shutil.copyfileobj(response.raw, f)
+
+        HistoryManager.append_to_download_history(result['Details'], result['TrackerId'], search_history)
 
     @staticmethod
     def _sanitize_name(release_name):
@@ -219,15 +234,65 @@ class Downloader:
         return file_path
 
 
+class HistoryManager:
+    search_history_file_path = os.path.join( os.path.dirname(os.path.abspath(__file__)), 'SearchHistory.json' )
+    # Some trackers may have several proxies. This ensures that only the url path is logged
+    # eg. tracker1.proxy1.org/details?id=55 != tracker1.proxy9001.org/details?id=55, but '/details?id=55' remains the same
+    url_path_re = r'^https?://[^/]+(.+)'
+
+    @staticmethod
+    def get_download_history():
+        try:
+            with open(HistoryManager.search_history_file_path, 'r', encoding='utf8') as f:
+                search_history = json.load(f)
+            return search_history
+        except:
+            return {
+                'basenames_searched': [],
+                'download_history': {}
+            }
+
+    @staticmethod
+    def is_file_previously_searched(basename, search_history):
+        for name in search_history['basenames_searched']:
+            if basename == name:
+                return True
+        return False
+
+    @staticmethod
+    def is_torrent_previously_grabbed(result, search_history):
+        url_path = re.search( HistoryManager.url_path_re, result['Details'] ).group(1)
+        tracker_id = result['TrackerId']
+
+        if search_history['download_history'].get( tracker_id, None ) is None:
+            return False
+
+        for download_history_url_path in search_history['download_history'][tracker_id]:
+            if download_history_url_path == url_path:
+                return True
+        return False
+
+    @staticmethod
+    def append_to_download_history(details_url, tracker_id, search_history):
+        url_path = re.search(HistoryManager.url_path_re, details_url).group(1)
+
+        if search_history['download_history'].get(tracker_id, None) is None:
+            search_history['download_history'][tracker_id] = []
+
+        search_history['download_history'][tracker_id].append(url_path)
+
+
 def main():
     assert_settings()
     paths = [ os.path.normpath(ARGS.input_path)] if not ARGS.parse_dir else [os.path.join(ARGS.input_path, f) for f in os.listdir(ARGS.input_path) ]
 
+    search_history = HistoryManager.get_download_history()
+
     for i, path in enumerate(paths):
         local_release_data = ReleaseData.get_release_data(path)
 
-        info = '\nSearching for {num} of {size}: {basename} / {title} {year}'.format(
-            num=i + 1, 
+        info = 'Searching for {num} of {size}: {basename} / {title} {year}'.format(
+            num=i + 1,
             size=len(paths), 
             basename=os.path.basename(path), 
             title=local_release_data['guessed_data']['title'], 
@@ -242,12 +307,18 @@ def main():
             continue
 
         searcher = Searcher()
-        matching_results = searcher.search(local_release_data)
-        # debug
+        matching_results = searcher.search(local_release_data, search_history)
+
+        ###
         # [print(f['Title']) for f in matching_results]
         for result in matching_results:
-            Downloader.download(result)
+            Downloader.download(result, search_history)
+
         time.sleep(ARGS.delay)
+
+    # write back to download history file
+    with open(HistoryManager.search_history_file_path, 'w', encoding='utf8') as f:
+        json.dump(search_history, f, indent=4)
     
 
 def assert_settings():
